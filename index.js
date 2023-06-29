18765f6075c408072de8b199802eb0bf4f2529bb
Jordan Choo <jordan.choo@kogneta.com>
2023-06-29 19:45:58 -0400

    Created commented workflow for main function

--- index.js
+++ index.js
@@ -17,6 +17,36 @@ const bigQuery = new BigQuery({
 modules.exports = {
 
     main: async(req,res) => {
+        
+        const seBots = [
+            {
+                name: 'googleBot',
+                url: 'https://developers.google.com/static/search/apis/ipranges/googlebot.json'
+            },
+            {
+                name: 'bingBot',
+                url: 'https://www.bing.com/toolbox/bingbot.json'
+            }
+        ]
+
+        // Get all of the existing IPs
+        const existingIps = await ExistingIps();
+
+        // Go through each of the seBots
+        for (const seBot of seBots) {
+            // Download the JSON from the URL
+
+            // Loop through each item in the JSON
+                // Check to see if the IP address exists in the existingIps
+                // If not add it to missingIps
+        }
+
+        // Check to see the missingIps legnth
+            // If more than 0 push the rows to BQ
+
+    },
+
+    getExistingIps: async(req,res) => {
 
     },
 
